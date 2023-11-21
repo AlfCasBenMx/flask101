7940178ca8a0189ad3860755228b37128304df3e
AlfCasBenMx <alfcasben@gmail.com>
2023-11-21 11:25:02 -0600

    sesion template folder

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+
+from flask import Flask, render_template
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
@@ -14,31 +15,18 @@ def plotly_chart():
         'C': [7, 8, 9]
     })
 
-    # Convertir el DataFrame a HTML y almacenarlo en una variable.
+    # Convertir el DataFrame a HTML.
     df_html = df.to_html()
 
     # Crear una figura con Plotly.
     fig = px.bar(df, x=df.index, y=['A', 'B', 'C'], barmode='group', title="Sample Bar Chart")
 
     # Convertir la figura a HTML y desactivar la inclusión de Plotly.js
-    # ya que lo incluiremos por separado en el HTML final.
     fig_html = pio.to_html(fig, full_html=False, include_plotlyjs=False)
 
-    # Construir la respuesta HTML final, incluyendo Plotly.js solo una vez.
-    return f"""
-    <html>
-        <head>
-            <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-        </head>
-        <body>
-            {df_html} <!-- Coloca la tabla por encima de la gráfica -->
-            <hr> <!-- Una línea horizontal como separador -->
-            {fig_html} <!-- La gráfica de barras viene después de la tabla -->
-        </body>
-    </html>
-    """
+    # Render the template with DataFrame and Plotly chart HTML
+    return render_template('index.html', df_html=df_html, fig_html=fig_html)
 
 # Aquí iría el código para ejecutar con waitress si lo necesitas.
-# Por ejemplo:
 # from waitress import serve
 # serve(app, host='0.0.0.0', port=8080)

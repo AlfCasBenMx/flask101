f33ede3e584e3f87eb21835588b894745544ee4e
AlfCasBenMx <alfcasben@gmail.com>
2023-11-21 11:29:21 -0600

    ajemeplo ajax

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
@@ -26,6 +26,24 @@ def plotly_chart():
 
     # Render the template with DataFrame and Plotly chart HTML
     return render_template('index.html', df_html=df_html, fig_html=fig_html)
+
+@app.route('/update_chart')
+def update_chart():
+    # Crear un segundo DataFrame de pandas.
+    df2 = pd.DataFrame({
+        'A': [3, 2, 1],
+        'B': [6, 5, 4],
+        'C': [9, 8, 7]
+    })
+
+    # Crear una nueva figura con Plotly.
+    fig2 = px.bar(df2, x=df2.index, y=['A', 'B', 'C'], barmode='group', title="Updated Bar Chart")
+
+    # Convertir la nueva figura a HTML.
+    fig_html = pio.to_html(fig2, full_html=False, include_plotlyjs=False)
+
+    # Return the HTML for the updated chart
+    return jsonify({'fig_html': fig_html})
 
 # Aquí iría el código para ejecutar con waitress si lo necesitas.
 # from waitress import serve
